837d51a4a11e0db3100b1383519933b91781fdc1
lud <ludovic@demblans.com>
2026-05-10 16:38:17 +0200

    chore(deps): Updated all dev dependencies

--- mix.exs
+++ mix.exs
@@ -84,7 +84,6 @@ defmodule Nvir.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
-      {:doctor, "~> 0.22.0", only: :dev, runtime: false},
       {:readmix, "~> 0.3", only: [:dev, :test], runtime: false}
     ]
   end
